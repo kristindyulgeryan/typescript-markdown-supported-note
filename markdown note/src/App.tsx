f8c6e05ff0e0b61be1e6a7569bf9d6f66e54ad40
Kristin <kristindyulgeryan@abv.bg>
2026-05-12 14:10:06 +0300

    add delete functionality to Note component

--- markdown note/src/App.tsx
+++ markdown note/src/App.tsx
@@ -75,6 +75,12 @@ function App() {
     });
   }
 
+  function onDeleteNote(id: string) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -98,7 +104,7 @@ function App() {
             }
           />
           <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-            <Route index element={<Note />} />
+            <Route index element={<Note onDelete={onDeleteNote} />} />
             <Route
               path="edit"
               element={
