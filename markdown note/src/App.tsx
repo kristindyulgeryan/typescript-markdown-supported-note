d1ab944b1ab9835d656e959ab3478753b39e3294
Kristin <kristindyulgeryan@abv.bg>
2026-05-12 11:07:24 +0300

    add EditNote component

--- markdown note/src/App.tsx
+++ markdown note/src/App.tsx
@@ -9,6 +9,7 @@ import { v4 as uuidV4 } from "uuid";
 import NoteList from "./components/NoteList";
 import NoteLayout from "./components/NoteLayout";
 import Note from "./components/Note";
+import EditNote from "./components/EditNote";
 
 export type Note = {
   id: string;
@@ -57,6 +58,23 @@ function App() {
     });
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            ...data,
+
+            tagIds: tags.map((tag) => tag.id),
+          };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -81,7 +99,16 @@ function App() {
           />
           <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
             <Route index element={<Note />} />
-            <Route path="edit" element={<h1>edit</h1>} />
+            <Route
+              path="edit"
+              element={
+                <EditNote
+                  onSubmit={onUpdateNote}
+                  onAddTag={addTag}
+                  availableTags={tags}
+                />
+              }
+            />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
